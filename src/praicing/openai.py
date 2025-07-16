0e6dc16c9ec0661ead7b274d9e48d2486e7f5fdb
João Palmeiro <joaommpalmeiro@gmail.com>
2025-07-16 19:11:31 +0100

    Add second function

--- src/praicing/openai.py
+++ src/praicing/openai.py
@@ -8,6 +8,11 @@ BASE_TOKENS: dict[str, int] = {"gpt-4o-2024-08-06": 85}
 
 TILE_TOKENS: dict[str, int] = {"gpt-4o-2024-08-06": 170}
 
+PATCH_SIZE = 32
+MAX_PATCHES = 1536
+
+MULTIPLIERS = {"gpt-4.1-mini-2025-04-14": 1.62, "gpt-4.1-nano-2025-04-14": 2.46}
+
 
 def count_tokens_for_image_with_detail(
     image: Path, model: str, detail: Literal["low", "high"]
@@ -41,5 +46,37 @@ def count_tokens_for_image_with_detail(
     # "4. Add the base tokens to the total"
     tiles_tokens = total_tiles * TILE_TOKENS[model]
     total_tokens = tiles_tokens + BASE_TOKENS[model]
+
+    return total_tokens
+
+
+def count_tokens_for_image(image: Path, model: str) -> int:
+    with Image.open(image) as img:
+        width, height = img.size
+
+    # "A. Calculate the number of 32px x 32px patches that are needed to fully cover the image"
+    raw_patches_width = math.ceil(width / PATCH_SIZE)
+    raw_patches_height = math.ceil(height / PATCH_SIZE)
+    raw_patches = raw_patches_width * raw_patches_height
+
+    # "B. If the number of patches exceeds 1536, we scale down the image so that it can be covered by no more than 1536 patches"
+    if raw_patches > MAX_PATCHES:
+        r = math.sqrt((PATCH_SIZE**2) * MAX_PATCHES / (width * height))
+
+        r = r * min(
+            math.floor(width * r / PATCH_SIZE) / (width * r / PATCH_SIZE),
+            math.floor(height * r / PATCH_SIZE) / (height * r / PATCH_SIZE),
+        )
+
+        width = width * r
+        height = height * r
+
+    # "C. The token cost is the number of patches, capped at a maximum of 1536 tokens"
+    patches_width = math.ceil(width / PATCH_SIZE)
+    patches_height = math.ceil(height / PATCH_SIZE)
+    patches = patches_width * patches_height
+    patches = min(patches, MAX_PATCHES)
+
+    total_tokens = math.ceil(patches * MULTIPLIERS[model])
 
     return total_tokens
